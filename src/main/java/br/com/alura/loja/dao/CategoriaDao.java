66e45e61f83d3aa55bee273ee94eec54bee4581f
WillianFarias <williansousafarias@gmail.com>
2022-12-12 22:34:57 -0300

    Criacao da DAO de Categoria

---
+++ src/main/java/br/com/alura/loja/dao/CategoriaDao.java
@@ -0,0 +1,36 @@
+package br.com.alura.loja.dao;
+
+import br.com.alura.modelo.Categoria;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class CategoriaDao {
+
+    private EntityManager manager;
+
+    public CategoriaDao(EntityManager manager) {
+        this.manager = manager;
+    }
+
+    public void cadastrar(Categoria categoria) {
+        this.manager.persist(categoria);
+    }
+
+    public void atualizar(Categoria categoria) {
+        this.manager.merge(categoria);
+    }
+
+    public void remover(Categoria categoria) {
+        categoria = this.manager.merge(categoria);
+        this.manager.remove(categoria);
+    }
+
+    public Categoria buscarPorId(Long id) {
+        return this.manager.find(Categoria.class, id);
+    }
+
+    public List<Categoria> buscarTodos() {
+        return this.manager.createQuery("SELECT c FROM Categoria c").getResultList();
+    }
+}
